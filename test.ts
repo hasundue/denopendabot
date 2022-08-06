5f47befb8460a010e6a2befe4963adb8ee20dc0c
Shun Ueda <hasundue@gmail.com>
2022-08-06 17:59:07 +0900

    chore(test): Remove conflict markers

--- test.ts
+++ test.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
-import { main } from "./mod.ts";
-=======
-import { assertEquals } from "https://deno.land/std@0.148.0/testing/asserts.ts";
 import { main } from "./main.ts";
->>>>>>> bc5586b (build: rename mod.ts as main.ts)
 
 Deno.test("hello()", () => {
   assertEquals(
