c99e3869cb49669f5b649d784507e960bfc384f9
denopendabot <denopendabot@gmail.com>
2022-10-11 12:12:39 +0900

    build(deps): bump deno.land/std@0.158.0/testing/asserts.ts to 0.159.0

--- lib/common_test.ts
+++ lib/common_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 import { pullRequestType } from "./common.ts";
 import { Update } from "./repo.ts";
 
