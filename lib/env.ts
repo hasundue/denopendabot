06d69fd4726d52b91d4b69d088742cbaa368e561
hasundue <hasundue@gmail.com>
2022-10-12 21:20:42 +0900

    fix(env): do not require --allow-read in CI and Deploy

--- lib/env.ts
+++ lib/env.ts
@@ -1,16 +1,29 @@
-import "https://deno.land/std@0.159.0/dotenv/load.ts";
+import { configSync } from "https://deno.land/std@0.159.0/dotenv/mod.ts";
 
 type Env = {
   CI?: string;
   DENO_DEPLOYMENT_ID?: string;
+
   GH_TOKEN?: string;
   GITHUB_TOKEN?: string;
+
   APP_ID: string;
   CLIENT_ID: string;
   CLIENT_SECRET: string;
   WEBHOOK_SECRET: string;
+
   UPSTASH_REDIS_REST_URL: string;
   UPSTASH_REDIS_REST_TOKEN: string;
 };
 
-export const env: Env = Deno.env.toObject() as Env;
+const ENV = Deno.env.toObject();
+
+function getEnv() {
+  if (ENV["CI"] || ENV["DENO_DEPLOYMENT_ID"]) {
+    return ENV;
+  } else {
+    return configSync();
+  }
+}
+
+export const env = getEnv() as Env;
