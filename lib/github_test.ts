fec94853544f46bc72d05212301e0ea87b6f5ca9
denopendabot <denopendabot@gmail.com>
2022-10-13 16:33:37 +0900

    docs(deps): bump hasundue/denopendabot from 0.5.2 to 0.5.3

--- lib/github_test.ts
+++ lib/github_test.ts
@@ -47,7 +47,7 @@ Deno.test({
 
     const update = new Update("mod.ts", {
       name: "hasundue/denopendabot",
-      initial: "0.5.2", // @denopendabot hasundue/denopendabot
+      initial: "0.5.3", // @denopendabot hasundue/denopendabot
       target: "1.0.0",
     });
     const message = update.message();
