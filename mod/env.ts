3ce38a3701d2dc31f6437ba99abbdfa7fc3fdad0
hasundue <hasundue@gmail.com>
2022-10-26 23:35:03 +0900

    fix(env): add DENO_DEPLOYMENT_ID in mod/env.ts

--- mod/env.ts
+++ mod/env.ts
@@ -1,12 +1,13 @@
 import { configSync } from "https://deno.land/std@0.160.0/dotenv/mod.ts";
 
 type Env = {
-  CI?: string;
   GITHUB_TOKEN?: string;
+  CI?: string;
+  DENO_DEPLOYMENT_ID?: string;
 };
 
 const getEnv = () => {
-  if (Deno.env.get("CI")) {
+  if (Deno.env.get("CI") || Deno.env.get("DENO_DEPLOYMENT_ID")) {
     return Deno.env.toObject() as Env;
   } else {
     return configSync({ export: true }) as Env;
