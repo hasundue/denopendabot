28cc33b9aacf8079c3145b0c882cb3dc5e913fa3
hasundue <hasundue@gmail.com>
2022-10-28 08:44:44 +0900

    test(mod): use src/deps.ts instead of src/version.ts

--- integration/mod_test.ts
+++ integration/mod_test.ts
@@ -5,14 +5,11 @@ import {
 import { env } from "../mod/env.ts";
 import { createCommits, createPullRequest, getUpdates } from "../mod.ts";
 import { GitHubClient } from "../mod/octokit.ts";
-import { VERSION } from "./src/version.ts";
 
 const github = new GitHubClient(env.GITHUB_TOKEN);
 
 const repo = "hasundue/denopendabot";
 const baseBranch = "test";
-const initial = VERSION;
-const target = "1.0.0";
 
 Deno.test("integration (module)", async () => {
   await github.createBranch(repo, baseBranch);
@@ -22,25 +19,23 @@ Deno.test("integration (module)", async () => {
   const options = {
     baseBranch,
     workingBranch,
-    release: target,
-    include: ["integration/src/version.ts"],
+    include: ["integration/src/deps.ts"],
     labels: ["test"],
   };
 
   const updates = await getUpdates(repo, options);
 
   assertEquals(updates.length, 1);
-  assertEquals(updates[0].path, "mod/version.ts");
-  assertEquals(updates[0].spec, { name: repo, initial, target });
+  assertEquals(updates[0].path, "integration/src/deps.ts");
+
+  assertEquals(updates[0].spec, {
+    name: "deno.land/x/dax",
+    initial: "0.14.0",
+    target: (await github.getLatestRelease("dsherret/dax"))!,
+  });
 
   await createCommits(repo, updates, options);
 
   const result = await createPullRequest(repo, options);
-
   assert(result);
-
-  assertEquals(
-    result.title,
-    `build(version): bump the version from ${initial} to ${target}`,
-  );
 });
---
+++ integration/src/deps.ts
@@ -0,0 +1,3 @@
+// deno-lint-ignore-file
+
+import $ from "https://deno.land/x/dax@0.14.0/mod.ts";
