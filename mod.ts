57a5dd7b88872e047fd819221428ef37207224b1
denopendabot <denopendabot@github.com>
2022-11-10 13:41:23 +0000

    build(deps): bump deno.land/std from 0.162.0 to 0.163.0

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-import { groupBy } from "https://deno.land/std@0.162.0/collections/group_by.ts";
-import { intersect } from "https://deno.land/std@0.162.0/collections/intersect.ts";
-import { withoutAll } from "https://deno.land/std@0.162.0/collections/without_all.ts";
+import { groupBy } from "https://deno.land/std@0.163.0/collections/group_by.ts";
+import { intersect } from "https://deno.land/std@0.163.0/collections/intersect.ts";
+import { withoutAll } from "https://deno.land/std@0.163.0/collections/without_all.ts";
 import { Octokit } from "https://esm.sh/@octokit/core@4.1.0";
 import { env } from "./mod/env.ts";
 import {
