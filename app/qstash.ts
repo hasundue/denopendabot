a7e679342721f1cc821b17b60366bf5c2ce7bf3e
denopendabot <denopendabot@github.com>
2022-11-12 00:02:29 +0000

    build(deps): bump deno.land/x/upstash_qstash from v0.1.8 to v0.2.0

--- app/qstash.ts
+++ app/qstash.ts
@@ -1,5 +1,5 @@
 import { crypto } from "https://deno.land/std@0.163.0/crypto/mod.ts";
-import { Receiver } from "https://deno.land/x/upstash_qstash@v0.1.8/pkg/receiver.ts";
+import { Receiver } from "https://deno.land/x/upstash_qstash@v0.2.0/pkg/receiver.ts";
 import { env } from "./env.ts";
 
 const QSTASH_URL = "https://qstash.upstash.io/v1";
