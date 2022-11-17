eddfc1bf59f0d425c436b30bff99d37392a0b852
denopendabot <denopendabot@github.com>
2022-11-17 00:02:10 +0000

    build(deps): bump deno.land/std from 0.164.0 to 0.165.0

--- app/qstash.ts
+++ app/qstash.ts
@@ -1,4 +1,4 @@
-import { crypto } from "https://deno.land/std@0.164.0/crypto/mod.ts";
+import { crypto } from "https://deno.land/std@0.165.0/crypto/mod.ts";
 import { Receiver } from "https://deno.land/x/upstash_qstash@v0.3.0/pkg/receiver.ts";
 import { env } from "./env.ts";
 
--- mod/common_test.ts
+++ mod/common_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 import { pullRequestType, removeIgnore } from "./common.ts";
 
 Deno.test("pullRequestType", () => {
