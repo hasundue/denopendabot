85a348980690cc74718d51ebce34105b086e8bc7
denopendabot <denopendabot@github.com>
2023-01-20 00:02:00 +0000

    build(deps): bump std from 0.172.0 to 0.173.0

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.172.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 import { Hono } from "https://deno.land/x/hono@v2.7.3/mod.ts";
 import { deployment, location } from "./app/deployments.ts";
 import { handler } from "./app/webhooks.ts";
