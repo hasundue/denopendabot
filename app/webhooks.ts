d99bb19ae754bb3edea20cbbe45bd9c1bb0fa69c
denopendabot <denopendabot@github.com>
2023-03-10 00:02:22 +0000

    build(deps): bump @octokit/app from 13.1.1 to 13.1.2

--- app/webhooks.ts
+++ app/webhooks.ts
@@ -1,6 +1,6 @@
 import { intersect } from "https://deno.land/std@0.178.0/collections/intersect.ts";
 import { Octokit } from "https://esm.sh/@octokit/core@4.2.0";
-import { App } from "https://esm.sh/@octokit/app@13.1.1";
+import { App } from "https://esm.sh/@octokit/app@13.1.2";
 import { EmitterWebhookEventName } from "https://esm.sh/@octokit/webhooks@10.7.0";
 import { env } from "./env.ts";
 import { Deployment, deployment } from "./deployments.ts";
