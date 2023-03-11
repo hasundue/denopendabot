9cb4d7a3bc6d93058d4d629ac6427cb70fc48aa5
denopendabot <denopendabot@github.com>
2023-03-11 00:01:48 +0000

    build(deps): bump std from 0.178.0 to 0.179.0

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-import { groupBy } from "https://deno.land/std@0.178.0/collections/group_by.ts";
-import { intersect } from "https://deno.land/std@0.178.0/collections/intersect.ts";
-import { withoutAll } from "https://deno.land/std@0.178.0/collections/without_all.ts";
+import { groupBy } from "https://deno.land/std@0.179.0/collections/group_by.ts";
+import { intersect } from "https://deno.land/std@0.179.0/collections/intersect.ts";
+import { withoutAll } from "https://deno.land/std@0.179.0/collections/without_all.ts";
 import { Octokit } from "https://esm.sh/@octokit/core@4.2.0";
 import { env } from "./mod/env.ts";
 import {
