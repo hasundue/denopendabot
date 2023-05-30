d4dfdc5d2945256b0536b82f5dc71969e318948a
denopendabot <denopendabot@github.com>
2023-05-30 00:01:30 +0000

    build(deps): bump hono from v3.1.8 to v3.2.3

--- app/webhooks.ts
+++ app/webhooks.ts
@@ -2,7 +2,7 @@ import { intersect } from "https://deno.land/std@0.190.0/collections/intersect.t
 import { Octokit } from "https://esm.sh/@octokit/core@4.1.0#~";
 import { App } from "https://esm.sh/@octokit/app@13.1.2#=";
 import { EmitterWebhookEventName } from "https://esm.sh/@octokit/webhooks@11.0.0";
-import { HonoRequest } from "https://deno.land/x/hono@v3.1.8/mod.ts";
+import { HonoRequest } from "https://deno.land/x/hono@v3.2.3/mod.ts";
 import { env } from "./env.ts";
 import { Deployment, deployment } from "./deployments.ts";
 import * as mod from "../mod.ts";
