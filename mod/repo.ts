31e93610f79d6a6d2687e16d65d7e2f576eea34c
denopendabot <denopendabot@github.com>
2023-10-13 00:01:55 +0000

    build(deps): bump std from 0.203.0 to 0.204.0

--- mod/repo.ts
+++ mod/repo.ts
@@ -1,4 +1,4 @@
-import { gt, parse } from "https://deno.land/std@0.203.0/semver/mod.ts";
+import { gt, parse } from "https://deno.land/std@0.204.0/semver/mod.ts";
 import { GitHubClient } from "./octokit.ts";
 import { semverRegExp, Update, UpdateSpec } from "./common.ts";
 import { env } from "./env.ts";
