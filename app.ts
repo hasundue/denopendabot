76b7621905340318091a8adff0d37519c3d946bf
denopendabot <denopendabot@github.com>
2023-10-19 00:02:02 +0000

    build(deps): bump hono from v3.7.6 to v3.8.1

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.204.0/http/server.ts";
-import { Hono } from "https://deno.land/x/hono@v3.7.6/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v3.8.1/mod.ts";
 import { getDeployEnvUrl, getThisDeployEnv } from "./app/deployments.ts";
 import { handler } from "./app/webhooks.ts";
 
